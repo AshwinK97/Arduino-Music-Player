5c2c1df07987fadf355ce665133c0736ab84ef02
AshwinK97 <ashwin.kamalakannan@ontariotechu.net>
2019-12-04 19:37:46 -0500

    added report

--- musicplayer.ino
+++ musicplayer.ino
@@ -2,6 +2,7 @@
 #include "TMRpcm.h"
 #include "SPI.h"
 
+// pin for sd card
 #define SD_ChipSelectPin 4
 TMRpcm music;
 
@@ -11,9 +12,11 @@ boolean debounce2 = true;
 boolean play_pause;
 
 void setup() {
+  // set pin for audio output
   music.speakerPin = 9;
-  Serial.begin(9600);
+  Serial.begin(9600); // allow serial output
 
+  // set pin for sd card
   if (!SD.begin(SD_ChipSelectPin)) {
     Serial.println("SD fail");
     return;
@@ -21,34 +24,41 @@ void setup() {
     Serial.println("SD success");
   }
 
+  // set pins for button input
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   
+  // set player configuration
   music.setVolume(5);
   music.quality(1);
   //music.volume(0);
 }
 
 void loop() {
+  
+  // read for when next button is pressed
   if (digitalRead(2) == LOW  && debounce1 == true) {
     Serial.println("NEXT");
-    song_number++;
-    if (song_number == 3)
+    song_number++; // incremement song
+    if (song_number == 3) // loop back if last song is reached
       song_number = 1;
-    debounce1 = false;
+    debounce1 = false; // set debounce false so button cannot be registered again
     
+    // play song based on song number
     if (song_number == 1)
       music.play("1.wav", 1);
     if (song_number == 2)
       music.play("2.wav", 1);
     
+    // read for when pause button is pressed
     if (digitalRead(3) == LOW  && debounce2 == true) {
       Serial.println("PLAY / PAUSE");
       music.pause();
-      debounce2 = false;
+      debounce2 = false; // set debounce false so button cannot be registered again
     }
     
+    // if switches are released, set debounce to true to allow for button press
     if (digitalRead(2) == HIGH)
       debounce1 = true;
     if (digitalRead(3) == HIGH)
